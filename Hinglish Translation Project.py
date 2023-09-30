f9b348cd559acc4faca986938627c11010f087fe
Thomas George <120734357+ThomasGeorgeGit@users.noreply.github.com>
2023-09-30 15:59:04 +0530

    Add files via upload

---
+++ Hinglish Translation Project.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from googletrans import Translator
+
+
+# In[5]:
+
+
+translater = Translator()
+
+out= translater.translate("Definitely share your feedback in the comment section.",dest='hi')
+print(out)
+
+
+# In[6]:
+
+
+translater = Translator()
+
+out= translater.translate("So even if it's a big video, I will clearly mention all the products.",dest='hi')
+print(out)
+
+
+# In[7]:
+
+
+translater = Translator()
+
+out= translater.translate(" I was waiting for my bag.",dest='hi')
+print(out)
+
+
+# In[ ]:
+
+
+
+
